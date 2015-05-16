e0c0efe26bf77115c0673ca921d7a13b31d1a402
pablo8421 <pablo_jsd-8@hotmail.com>
2015-05-15 23:53:23 -0600

    Ahora muestra la tabla y sus columnas con sus respectivos tipos :)

--- CRM/EsquemaTabla.cs
+++ CRM/EsquemaTabla.cs
@@ -9,16 +9,46 @@ namespace CRM
 {
     class EsquemaTabla
     {
+        string nombre;
+
+        List<String> columnas;
+        List<String> tipos;
+
         public EsquemaTabla(string nombre)
         {
+            this.nombre = nombre;
+            columnas = new List<string>();
+            tipos = new List<string>();
+
             //Query para obtener las columnas de la tabla en la BD
             DataTable dt = Control_query.querySelect("SELECT * FROM information_schema.columns WHERE table_name ='" + nombre + "';");
-
-            Console.WriteLine(nombre);
             
             foreach (DataRow row in dt.Rows)
             {
-                Console.WriteLine("\t" + row["column_name"]);
+                //Guardar el nombre de la columna
+                columnas.Add(row["column_name"].ToString());
+
+                //Obtener el tipo de la columna
+                string tipo = row["data_type"].ToString();
+                if (tipo.Equals("character varying"))
+                {
+                    tipo = "varchar, " + row["character_maximum_length"].ToString() + "";
+                }
+
+                //Guardar el tipo de la columna
+                tipos.Add(tipo);
+                
+            }
+
+            mostrarEnConsola();
+        }
+
+        public void mostrarEnConsola() {
+
+            Console.WriteLine(nombre);
+            for (int i = 0; i < columnas.Count; i++)
+            {
+                Console.WriteLine("\t" + columnas[i] + " - " + tipos[i]);
             }
         }
     }
