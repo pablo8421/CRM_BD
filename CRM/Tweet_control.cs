a5e9b8397b6ea35721142d2e319048eebc8c0275
pablo8421 <pablo_jsd-8@hotmail.com>
2015-05-16 18:12:39 -0600

    Tweet control tiene la opcion de obtener la cantidad de tweets deseada

--- CRM/Tweet_control.cs
+++ CRM/Tweet_control.cs
@@ -19,17 +19,30 @@ namespace CRM
 
         public Tweetinvi.Core.Interfaces.ITweet[] getTweets(String cuenta)
         {
+            return getTweets(cuenta, 100);
+        } 
+
+        public Tweetinvi.Core.Interfaces.ITweet[] getTweets(String cuenta, int cantidad)
+        {
             //Obtener el usuario
             Tweetinvi.Core.Interfaces.IUser user = Tweetinvi.User.GetUserFromScreenName(cuenta);
 
             //Generar la busqueda
-            var timelineParameter = Timeline.CreateUserTimelineRequestParameter(user);
+            Tweetinvi.Core.Interfaces.Models.Parameters.IUserTimelineRequestParameters timelineParameter = Timeline.CreateUserTimelineRequestParameter(user);
+
+            //Cantidad de tweets a obtener
+            timelineParameter.MaximumNumberOfTweetsToRetrieve = cantidad;
 
             //Obtener los tweets
             var tweets = Timeline.GetUserTimeline(timelineParameter);
             
             //Filtrar por los publicados por el usuario
             Tweetinvi.Core.Interfaces.ITweet[] tweetsPublicados = tweets.Where(x => x.Creator.Equals(user)).ToArray();
+
+            foreach (Tweetinvi.Core.Interfaces.ITweet tweet in tweetsPublicados)
+            {
+                Console.WriteLine(tweet.Text);
+            }
 
             return tweetsPublicados;
         }
